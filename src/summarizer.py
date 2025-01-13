207525ddf3520260dbcb04e2f8922b97ebb1bd30
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-13 12:59:10 +0330

    model

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.utils import read_config
 
@@ -8,4 +8,6 @@ config = read_config()
 model_checkpoint = config['model']['model_checkpoint']
 save_dir = config['model']['save_dir']
 
+
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
