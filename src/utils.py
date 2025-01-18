1b783a4f85231ba2bbd956cbe2405a9f25d760c6
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-18 09:04:57 +0330

    metrics class

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 from datasets import load_dataset
 import configparser
+import evaluate
+from nltk.tokenize import sent_tokenize
 from transformers import DataCollatorForTokenClassification
 
 
@@ -40,44 +42,44 @@ class preprocess:
         self.tokenizer = tokenizer
         self.max_input_length = max_input_length
         self.max_target_length = max_target_length
-    
+ 
     def _preprocess_func(self, examples):
         model_inputs = self.tokenizer(
             examples["article"],
+            max_length=self.max_input_length
             truncation=True,
         )
         labels = self.tokenizer(
-            examples["abstract"], truncation=True
+            examples["abstract"], max_length=self.max_target_length, truncation=True
         )
         model_inputs["labels"] = labels["input_ids"]
         return model_inputs
     
-    def map_preprocess_func(self, examples):
-        return self._preprocess_func(
-            examples,
-            self.tokenizer,
-            self.max_input_length,
-            self.max_target_length,
-        )
-    
     def __call__(self, dataset):
-        return dataset.map(self.map_preprocess_func, batched=True)
+        return dataset.map(self._preprocess_func, batched=True)
 
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    # Decode generated summaries into text
-    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    # Replace -100 in the labels as we can't decode them
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    # Decode reference summaries into text
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-    # ROUGE expects a newline after each sentence
-    decoded_preds = ["\n".join(sent_tokenize(pred.strip())) for pred in decoded_preds]
-    decoded_labels = ["\n".join(sent_tokenize(label.strip())) for label in decoded_labels]
-    # Compute ROUGE scores
-    result = rouge_score.compute(
-        predictions=decoded_preds, references=decoded_labels, use_stemmer=True
-    )
-    # Extract the median scores
-    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
-    return {k: round(v, 4) for k, v in result.items()}
+
+
+class Metrics:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.rouge_score = evaluate.load("rouge")
+
+    def compute_metrics(self, eval_pred):
+        predictions, labels = eval_pred
+        # Decode generated summaries into text
+        decoded_preds = self.tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        # Replace -100 in the labels as we can't decode them
+        labels = np.where(labels != -100, labels, self.tokenizer.pad_token_id)
+        # Decode reference summaries into text
+        decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
+        # ROUGE expects a newline after each sentence
+        decoded_preds = ["\n".join(sent_tokenize(pred.strip())) for pred in decoded_preds]
+        decoded_labels = ["\n".join(sent_tokenize(label.strip())) for label in decoded_labels]
+        # Compute ROUGE scores
+        result = self.rouge_score.compute(
+            predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+        )
+        # Extract the median scores
+        result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+        return {k: round(v, 4) for k, v in result.items()}
