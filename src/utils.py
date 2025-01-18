285283de0e7f840f01a4d55808ce33a80df54a23
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-18 08:47:56 +0330

    preprocess class

--- src/utils.py
+++ src/utils.py
@@ -29,20 +29,55 @@ def read_config(config_file='config.ini'):
     
     return config_dict
 
-def preprocess(
-    examples,
-    tokenizer,
-    max_input_length,
-    max_target_length
-    ):
-    model_inputs = tokenizer(
-        examples["article"],
-        truncation=True,
-    )
-    labels = tokenizer(
-        examples["abstract"], truncation=True
-    )
-    model_inputs["labels"] = labels["input_ids"]
-    return model_inputs
+class preprocess:
+    def __init__(
+        self,
+        tokenizer,
+        max_input_length,
+        max_target_length
+        ):
 
+        self.tokenizer = tokenizer
+        self.max_input_length = max_input_length
+        self.max_target_length = max_target_length
+    
+    def _preprocess_func(self, examples):
+        model_inputs = self.tokenizer(
+            examples["article"],
+            truncation=True,
+        )
+        labels = self.tokenizer(
+            examples["abstract"], truncation=True
+        )
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+    
+    def __call__(self, dataset):
+        return dataset.map(
+            lambda a: self._preprocess_func(
+                a,
+                self.tokenizer,
+                self.max_input_length,
+                self.max_target_length,
+                ) ,
+            batched=True
+            )
 
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    # Decode generated summaries into text
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    # Decode reference summaries into text
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    # ROUGE expects a newline after each sentence
+    decoded_preds = ["\n".join(sent_tokenize(pred.strip())) for pred in decoded_preds]
+    decoded_labels = ["\n".join(sent_tokenize(label.strip())) for label in decoded_labels]
+    # Compute ROUGE scores
+    result = rouge_score.compute(
+        predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+    )
+    # Extract the median scores
+    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+    return {k: round(v, 4) for k, v in result.items()}
